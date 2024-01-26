7c8c8f134fff0d435a67ac5ed21fb7af10d37ad1
Griffen Mustion <gmustion35@gmail.com>
2024-01-26 15:04:32 -0600

    Read gmt by line

--- task1/task1.R
+++ task1/task1.R
@@ -12,9 +12,9 @@ gene_info <- gene_info[, c(2, 3, 5)]
 map <- hash()
 
 for (i in 1:nrow(gene_info)) {
-    # Note: To account for those symbols which appear as a symbol and synonym in
-    # different rows (e.g. JUNB), we will define the GeneID corresponding to the
-    # row in which a given symbol appears in the Symbol column as the "primary_ID."
+    # Note: To account for those symbols which appear as both a symbol and synonym in
+    # different rows (e.g. JUNB), we will define a given symbol's "primary_ID" as the
+    # GeneID corresponding to the row in which that symbol appears in the Symbol column.
     # All symbols will be mapped to their primary_ID if one exists.
 
     map[gene_info[[i, 2]]] <- gene_info[[i, 1]]
@@ -31,17 +31,17 @@ for (i in 1:nrow(gene_info)) {
 
 # Extract data from .gmt file
 gmt <- "h.all.v2023.1.Hs.symbols.gmt"
-pathways <- fread(gmt, header = FALSE, fill = TRUE, sep = "\t")
+lines <- readLines(gmt)
 
-# Loop through all symbols and replace them with the hash table
-for (i in 1:nrow(pathways)) {
-    for (j in 3:ncol(pathways[i, ])) {
-        sym = pathways[[i,j]]
-        if (sym != "") {
-            pathways[[i,j]] <- map[[sym]]
-        }
+pathways <- lapply(lines, function(line) {
+    line = strsplit(line, "\t")[[1]]
+
+    for (i in 3:length(line)) {
+        line[i] <- map[[line[i]]]
     }
-}
+
+    return(paste(unlist(line), collapse = "\t"))
+})
 
 # Write results
-fwrite(pathways, file = "output.tsv", sep = "\t", quote = FALSE, col.names=FALSE)
+writeLines(unlist(pathways), "output.gmt")
